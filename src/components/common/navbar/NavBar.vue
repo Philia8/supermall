3d04486b158314b3ab722a687e3c46899d41ac19
Philia8 <philia_liu@qq.com>
2021-03-04 17:27:37 +0800

    导航栏初始化

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+   <div class="nav-bar">
+       <div class="left"><slot name="left"></slot></div>
+       <div class="center"><slot name="center"></slot></div>
+       <div class="right"><slot name="right"></slot></div>
+   </div>
+</template>
+
+<script>
+    export default {
+        name: 'NavBar'
+    }
+</script>
+
+<style>
+    .nav-bar {
+        display: flex;
+        height: 44px;
+        line-height: 44px;
+    }
+    /* 左侧部分设置固定宽度 */
+    
+    .right .left {
+        width: 60px;
+        background-color: green;
+    }
+    
+    .center {
+        flex: 1;
+        background-color: hotpink;
+    }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,17 @@
 <template>
    <div id="Home">
+       <nav-bar></nav-bar>
        <h2>首页</h2>
    </div>
 </template>
 
 <script>
+    import NavBar from 'components/common/navBar/NavBar'
     export default {
-        name: 'Home'
+        name: 'Home',
+        components: {
+            NavBar
+        }
     }
 </script>
 
